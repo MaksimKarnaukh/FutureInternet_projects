107a580113522837265f31722eaf9e603685b407
MaksimKarnaukh <Maksim.Karnaukh@student.uantwerpen.be>
2024-11-27 16:59:47 +0100

    ex2

--- ex2/Skeleton-Lab-2.py
+++ ex2/Skeleton-Lab-2.py
@@ -9,6 +9,8 @@ from pox.lib.addresses import EthAddr
 from collections import namedtuple
 import os
 
+import csv
+
 #Please add the classes and methods you consider necessary
 
 
@@ -25,8 +27,42 @@ class Firewall(EventMixin):
         self.listenTo(core.openflow)
         log.debug("Activating Firewall")
 
+        self.blocked_mac_pairs = []
+
+        self.load_policies()
+
+    def load_policies(self):
+
+        try:
+            with open(policyFile, 'r') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    mac_0 = EthAddr(row['mac_0'])
+                    mac_1 = EthAddr(row['mac_1'])
+                    self.blocked_mac_pairs.append((mac_0, mac_1))
+                    log.debug("Blocking traffic between %s and %s", mac_0, mac_1)
+
+        except Exception as e:
+            log.error("Error loading firewall policies: %s", e)
+
     def _handle_ConnectionUp (self, event):    
-         #Please add your code here
+
+        #Please add your code here
+
+        for mac_0, mac_1 in self.blocked_pairs:
+            # Block traffic from mac_0 to mac_1
+            msg = of.ofp_flow_mod()
+            msg.match.dl_src = mac_0
+            msg.match.dl_dst = mac_1
+            msg.actions = []  # Drop packet
+            event.connection.send(msg)
+
+            # Block traffic from mac_1 to mac_0
+            msg = of.ofp_flow_mod()
+            msg.match.dl_src = mac_1
+            msg.match.dl_dst = mac_0
+            msg.actions = []  # Drop packet
+            event.connection.send(msg)
     
         log.debug("Installed rules in %s", dpidToStr(event.dpid))
 
